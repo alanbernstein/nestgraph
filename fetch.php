91365bcf3896a3824435819753af2db6a41906c3
Alan Bernstein <alan.aaron.bernstein@gmail.com>
2017-06-29 00:03:54 -0500

    Retrieve and store cooling state and outside temperature

--- fetch.php
+++ fetch.php
@@ -1,5 +1,7 @@
 <?php
 
+// SELECT from database for graph
+
 require 'inc/config.php';
 require 'inc/class.db.php';
 
@@ -10,16 +12,19 @@ if ($_GET["hrs"]) {
   $hrs = $_GET["hrs"];
 }
 
+// NOTE: in future installations, will need to change column order to match dbsetup (cooling after heating). i couldn't figure out how to change column order in this, and don't care enough to do it by creating a new table.
+
 try {
   $db = new DB($config);
   if ($stmt = $db->res->prepare("SELECT * from data where timestamp>=DATE_SUB(NOW(), INTERVAL ? HOUR) order by timestamp")) {
     $stmt->bind_param("i", $hrs);
     $stmt->execute();
-    $stmt->bind_result($timestamp, $heating, $target, $current, $humidity, $updated);
+    $stmt->bind_result($timestamp, $heating, $target, $current, $humidity, $updated, $cooling, $outside);
     header("Content-type: text/tab-separated-values");
-    print "timestamp\theating\ttarget\tcurrent\thumidity\tupdated\n";
+    //header("Content-type: text");
+    print "timestamp\theating\ttarget\tcurrent\thumidity\tupdated\tcooling\toutside\n";
     while ($stmt->fetch()) {
-      print implode("\t", array($timestamp, $heating, $target, $current, $humidity, $updated)) . "\n";
+      print implode("\t", array($timestamp, $heating, $target, $current, $humidity, $updated, $cooling, $outside)) . "\n";
     }
     $stmt->close();
   }
