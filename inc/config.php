769fb4fc901c0e854949087cf9d7d98323871701
Alan Bernstein <alan.aaron.bernstein@gmail.com>
2017-06-29 00:18:50 -0500

    Update example config with new info for notifications and weather API

--- inc/config.php
+++ inc/config.php
@@ -6,6 +6,11 @@ $config = array('db_ip' => '127.0.0.1',
 		'db_name' => 'nest',
 		'nest_user' => 'your_nest_username',
 		'nest_pass' => 'your_nest_password',
+		'domain' => 'your_domain',
+		'web_key' => 'some_password',
+		'email' => 'your_email',
+		'zip_code' => 'your_zip',
+		'country_code' => 'your_country',
 		'local_tz' => 'America/New_York' // see http://php.net/manual/en/timezones.php
 		);
 
